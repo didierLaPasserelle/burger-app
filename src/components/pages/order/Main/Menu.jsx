420a3dc66aa61eaee6d6318ab2deb13c66914f49
Delaplacette <delaplacette_didier@yahoo.fr>
2023-07-24 11:54:03 +0200

    F05: Add assets

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { useState } from 'react';
 import styled from 'styled-components';
 
 export default function Menu() {
+
+const [products, setProducts] = useState([])
+
   return (
     <MenuStyled>Menu</MenuStyled>
   )
