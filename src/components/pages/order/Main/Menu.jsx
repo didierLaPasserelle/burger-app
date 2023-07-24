4560edc81e0b2a661cc9000e99c45cdda9c29c07
Delaplacette <delaplacette_didier@yahoo.fr>
2023-07-24 13:24:05 +0200

    F05: ORDER > MENU > Add final style

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { useState } from "react";
 import { fakeMenu2 } from "../../../../fakeData/fakeMenu.js";
+import { theme } from "../../../../theme"
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2);
@@ -17,13 +18,13 @@ export default function Menu() {
 }
 
 const MenuStyled = styled.div`
-  background: purple;
-  display: grid;
+  background: ${theme.colors.background_white};  display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-row-gap: 60px;
   grid-column-gap: 60px;
   padding: 50px 50px 150px;
   justify-content: center;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   .produit {
     background: red;
