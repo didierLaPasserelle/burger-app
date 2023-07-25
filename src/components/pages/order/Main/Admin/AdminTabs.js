8d845c238da8f26364d096191482db8906dda76e
Delaplacette <delaplacette_didier@yahoo.fr>
2023-07-25 15:39:15 +0200

    F07: ADMIN > Add AdminTabs and AdminPanel

---
+++ src/components/pages/order/Main/Admin/AdminTabs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components';
+
+export default function AdminTabs() {
+  return (
+    <AdminStyled>
+        AdminTabs
+    </AdminStyled>
+  )
+}
+
+const AdminStyled = styled.div`
+  border: 1px solid red;
+`;
