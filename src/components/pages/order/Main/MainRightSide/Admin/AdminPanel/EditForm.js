797e4a3621af479d3941fe166cbe86db32c631fe
Delaplacette <delaplacette_didier@yahoo.fr>
2023-09-13 19:57:06 +0200

    F09:EditForm> Extract EditForm message

--- src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/EditForm.js
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/EditForm.js
@@ -5,6 +5,7 @@ import ImagePreview from "./ImagePreview";
 import TextInput from "../../../../../../reusable-ui/TextInput";
 import { getInputTextsConfig } from "./inputTextConfig";
 import { theme } from "../../../../../../../theme";
+import EditInfoMessage from "./EditInfoMessage";
 
 export default function EditForm() {
   const { cardClickedOn, setCardClickedOn, handleEdit, titleEditRef } =
@@ -40,10 +41,7 @@ export default function EditForm() {
         ))}
       </div>
       <div className="submit">
-        <span className="sentence">
-          Cliquer sur un produit pour le modifier{" "}
-          <span className="live-update">en temps réel</span>
-        </span>
+        <EditInfoMessage/>
       </div>
     </EditFormStyled>
   );
@@ -74,14 +72,5 @@ const EditFormStyled = styled.form`
     align-items: center;
     position: relative;
     top: 3px;
-
-    .sentence {
-      color: ${theme.colors.primary};
-      font-size: ${theme.fonts.size.SM};
-
-      .live-update {
-        text-decoration: underline;
-      }
-    }
   }
 `;
