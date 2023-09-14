cb494cfc20e0af89a460ef6e50a8cfe3cb32ce2b
Delaplacette <delaplacette_didier@yahoo.fr>
2023-09-14 18:39:24 +0200

    F10: Basket > Add Layout

--- src/components/pages/order/Main/Basket.js
+++ src/components/pages/order/Main/Basket.js
@@ -1,9 +1,34 @@
-import styled from "styled-components"
+import styled from "styled-components";
+import { theme } from "../../../../theme";
 
 export default function Basket() {
-  return <BasketStyled>Basket</BasketStyled>
+  return (
+    <BasketStyled>
+      <div className="header">
+        <p>Total</p>
+        <p>0,00</p>
+      </div>
+
+    <div className="body">Body</div>
+    <div className="footer">Footer</div>
+    </BasketStyled>
+  );
 }
 
 const BasketStyled = styled.div`
   background: pink;
-`
+  display: flex;
+  flex-direction: column;
+  
+  .header {
+   background: grey;
+  }
+
+  .body{
+    background: blue;
+  }
+
+  .footer{ 
+    background: green;
+  }
+`;
