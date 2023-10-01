ae8fdcc48d6d1357f46692b869d5c8db8155370d
Delaplacette <delaplacette_didier@yahoo.fr>
2023-10-01 18:50:22 +0200

    F13 : api > user > add getter and setter

---
+++ src/api/user.js
@@ -0,0 +1,25 @@
+import { doc, getDoc, setDoc } from "firebase/firestore"
+import { db } from "./firebase-config"
+import { fakeMenu } from "../fakeData/fakeMenu"
+
+export const getUser = async (idUser) => { 
+    const docRef = doc(db, "users", idUser) // La méthode doc() prend en param' le chemin pour accéder à la donnée qui nous intéresse.
+    const result =  await getDoc(docRef)
+
+    if(result.exists()){
+        const userReceived = result.data()
+        return userReceived
+    }
+ }
+
+ export const addUser = (userId) => { 
+    
+    const docRef= doc(db, "users", userId)
+
+    const newDoc = { 
+        username : userId,
+        menu: fakeMenu.SMALL
+    }
+
+    setDoc(docRef, newDoc)
+  }
