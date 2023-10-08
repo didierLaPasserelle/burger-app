3c9cccecc38f6929a8da87a8244e4a0cf91cc6e8
Delaplacette <delaplacette_didier@yahoo.fr>
2023-10-08 21:51:58 +0200

    F14: ADMIN > Panel > add animation onAppear

--- src/components/pages/order/Main/MainRightSide/MainRightSide.js
+++ src/components/pages/order/Main/MainRightSide/MainRightSide.js
@@ -1,25 +1,35 @@
-import { useContext } from "react"
-import styled from "styled-components"
-import OrderContext from "../../../../../context/OrderContext"
-import { theme } from "../../../../../theme"
-import Admin from "./Admin/Admin"
-import Menu from "./Menu/Menu"
+import { useContext } from "react";
+import styled, { css } from "styled-components";
+import OrderContext from "../../../../../context/OrderContext";
+import { theme } from "../../../../../theme";
+import Admin from "./Admin/Admin";
+import Menu from "./Menu/Menu";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { adminAnimation } from "../../../../../theme/animations";
 
 export default function MainRightSide() {
-  const { isModeAdmin } = useContext(OrderContext)
+  const { isModeAdmin } = useContext(OrderContext);
 
   return (
     <MainRightSideStyled>
       <Menu />
-      {isModeAdmin && <Admin />}
+      {isModeAdmin && (
+        <TransitionGroup className="transition-group">
+          <CSSTransition appear={true} classNames="admin" timeout={3000}>
+            <Admin />
+          </CSSTransition>
+        </TransitionGroup>
+      )}
     </MainRightSideStyled>
-  )
+  );
 }
+
 
 const MainRightSideStyled = styled.div`
   position: relative;
   overflow-y: hidden;
   display: grid;
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
-  
-`
+
+  ${adminAnimation}
+`;
