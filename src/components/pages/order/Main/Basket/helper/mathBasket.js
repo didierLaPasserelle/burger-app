649973608369f93cc26ee64765801ae576a8a715
Delaplacette <delaplacette_didier@yahoo.fr>
2023-10-09 20:15:38 +0200

    F14: remove a console.log

--- src/components/pages/order/Main/Basket/helper/mathBasket.js
+++ src/components/pages/order/Main/Basket/helper/mathBasket.js
@@ -2,9 +2,7 @@ import { replaceFrenchCommaWithDot } from "../../../../../utils/maths";
 
 export const calculateAmountToPay = (basket, menu) =>
   basket.reduce((total, basketItem) => {
-    console.log("menu: ", menu);
     const menuItem = menu.find((item) => item.id === basketItem.id);
-    console.log("menuItem: ", menuItem);
     if (isNaN(menuItem.price)) return total;
     if (menuItem) {
       total +=
