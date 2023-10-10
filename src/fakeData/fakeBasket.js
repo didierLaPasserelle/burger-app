6357a64eff47b70e6ca2a21347e1a2b584b87b4d
Delaplacette <delaplacette_didier@yahoo.fr>
2023-10-10 16:04:45 +0200

    F15 : Add assets and delete useless files

--- src/fakeData/fakeBasket.js
+++ src/fakeData/fakeBasket.js
@@ -1,24 +1,15 @@
-import { EMPTY_PRODUCT } from "../components/enums/product"
+import { EMPTY_PRODUCT } from "../components/enums/product";
 
-export const EMPTY = []
+export const EMPTY = [];
 
 export const SMALL = [
-  // {
-  //   id: "1",
-  //   title: "Burger Meal",
-  //   imageSource: "images/burger1.png",
-  //   quantity: 1,
-  // },
   {
     id: "1",
-    imageSource: "/images/burger-bacon-egg.png",
-    title: "Burger Smoke BBQ",
-    price: 5.598,
-    quantity: 0,
-    isAvailable: true,
-    isAdvertised: false,
+    title: "Burger Meal",
+    imageSource: "images/burger1.png",
+    quantity: 1,
   },
-]
+];
 
 export const MEDIUM = [
   {
@@ -37,7 +28,8 @@ export const MEDIUM = [
   },
   {
     id: "3",
-    imageSource: "https://www.ecomiam.com/images/Image/Frites-du-bistro-Code-Menlog.jpg",
+    imageSource:
+      "https://www.ecomiam.com/images/Image/Frites-du-bistro-Code-Menlog.jpg",
     title: "Burger House",
     price: 5,
     quantity: 4,
@@ -49,7 +41,7 @@ export const MEDIUM = [
     price: 5,
     quantity: 4,
   },
-]
+];
 
 export const LARGE = [
   {
@@ -59,7 +51,7 @@ export const LARGE = [
     price: 3.1678,
     quantity: 13,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "2",
@@ -68,7 +60,7 @@ export const LARGE = [
     price: 3.568,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "3",
@@ -77,7 +69,7 @@ export const LARGE = [
     price: 5.367,
     quantity: 5,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "4",
@@ -86,7 +78,7 @@ export const LARGE = [
     price: 5.4985,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "5",
@@ -95,7 +87,7 @@ export const LARGE = [
     price: 3.487,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "6",
@@ -104,7 +96,7 @@ export const LARGE = [
     price: 3.356,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "7",
@@ -113,7 +105,7 @@ export const LARGE = [
     price: 5.598,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "8",
@@ -122,7 +114,7 @@ export const LARGE = [
     price: 2.567,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "9",
@@ -131,7 +123,7 @@ export const LARGE = [
     price: 3.7,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "10",
@@ -140,14 +132,14 @@ export const LARGE = [
     price: 4.678,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
-]
+];
 
 export const LARGE_WEIRD = [
   {
     ...EMPTY_PRODUCT,
-    quantity: 1
+    quantity: 1,
   },
   {
     id: "1",
@@ -156,7 +148,7 @@ export const LARGE_WEIRD = [
     price: 3.1678,
     quantity: 13,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "2",
@@ -165,7 +157,7 @@ export const LARGE_WEIRD = [
     price: 3.568,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "3",
@@ -174,7 +166,7 @@ export const LARGE_WEIRD = [
     price: 0.0,
     quantity: 5,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "4",
@@ -183,7 +175,7 @@ export const LARGE_WEIRD = [
     price: 5.4985,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "5",
@@ -192,7 +184,7 @@ export const LARGE_WEIRD = [
     price: 3.487,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "6",
@@ -201,7 +193,7 @@ export const LARGE_WEIRD = [
     price: 3.356,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "7",
@@ -210,7 +202,7 @@ export const LARGE_WEIRD = [
     price: 5.598,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "8",
@@ -219,7 +211,7 @@ export const LARGE_WEIRD = [
     price: 2.567,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "9",
@@ -228,7 +220,7 @@ export const LARGE_WEIRD = [
     price: 3.7,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
   {
     id: "10",
@@ -237,9 +229,9 @@ export const LARGE_WEIRD = [
     price: 4.678,
     quantity: 1,
     isAvailable: true,
-    isAdvertised: false,
+    isPublicised: false,
   },
-]
+];
 
 export const fakeBasket = {
   EMPTY,
@@ -247,4 +239,4 @@ export const fakeBasket = {
   MEDIUM,
   LARGE,
   LARGE_WEIRD,
-}
+};
