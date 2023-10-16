a3c83b7b94fee97cab73ffef095d5af861cefd28
Delaplacette <delaplacette_didier@yahoo.fr>
2023-10-16 15:49:03 +0200

    F15 : BASKET > helper > Remove unavailable item from sumToPay()

--- src/components/pages/order/Main/Basket/helper/mathBasket.js
+++ src/components/pages/order/Main/Basket/helper/mathBasket.js
@@ -1,9 +1,11 @@
 import { replaceFrenchCommaWithDot } from "../../../../../utils/maths";
+import { convertStringToBoolean } from "../../../../../utils/string";
 
 export const calculateAmountToPay = (basket, menu) =>
   basket.reduce((total, basketItem) => {
     const menuItem = menu.find((item) => item.id === basketItem.id);
-    if (isNaN(menuItem.price)) return total;
+    console.log('menuItem: ', menuItem)
+    if (convertStringToBoolean(menuItem.isAvailable) === false || isNaN(menuItem.price)) return total;
     if (menuItem) {
       total +=
         replaceFrenchCommaWithDot(menuItem.price || 0) * basketItem.quantity;
