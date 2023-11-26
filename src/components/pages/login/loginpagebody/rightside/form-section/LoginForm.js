14c22cf628e861970d626a166cee9c952cafdea2
Delaplacette <delaplacette_didier@yahoo.fr>
2023-11-26 17:10:39 +0100

    F16 : form-section > create helper

--- src/components/pages/login/loginpagebody/rightside/form-section/LoginForm.js
+++ src/components/pages/login/loginpagebody/rightside/form-section/LoginForm.js
@@ -1,4 +1,4 @@
-import { linksConfig } from "../linksConfig";
+import { linksConfig } from "./linksConfig";
 import { CustomLink } from "../../../../../reusable-ui/CustomLink";
 import DemoForm from "./DemoForm";
 import ClientForm from "./ClientForm";
@@ -7,28 +7,32 @@ import { BsArrowRight } from "react-icons/bs";
 import { theme } from "../../../../../../theme";
 import styled from "styled-components";
 import { useState } from "react";
-import { NO_PASSWORD_MESSAGE } from "../../../../../../enums/product"
+import { NO_PASSWORD_MESSAGE } from "../../../../../../enums/product";
+import { checkIsInputEmpty, findDemoPasswordMessage } from "./helper/login-form"
 
-export default function LoginForm({ username, handleChange }) {
+export default function LoginForm({ username, setUsername, handleChange }) {
   const [isClicked, setIsClicked] = useState(true);
 
   const handleDemoFormClick = () => {
     setIsClicked(true);
+    setUsername({ businessName: ''}); 
   };
-
+  
   const handleClientFormClick = () => {
     setIsClicked(false);
+    setUsername({ businessName: '', password: '' });
   };
+  
+
   const linksTab = linksConfig({
     handleDemoFormClick,
     handleClientFormClick,
     isClicked,
   });
 
-  const demoPasswordMessage = linksTab.find(
-    (link) => link.text === "J'accède à la démo");
+  const isInputEmpty = checkIsInputEmpty(isClicked, username);
+  const demoPasswordMessage = findDemoPasswordMessage(linksTab);
 
-  const isInputEmpty = username.businessName === "" && username.password === "" ;
 
   return (
     <LoginFormStyled>
@@ -50,32 +54,39 @@ export default function LoginForm({ username, handleChange }) {
       ) : (
         <ClientForm username={username} handleChange={handleChange} />
       )}
-      <Button disabled={isInputEmpty} label={"J'accède à mon espace"} Icon={<BsArrowRight />} />
+      <Button
+        disabled={isInputEmpty}
+        label={isClicked ? "J'accède à la démo" : "J'accède à mon espace"}
+        Icon={<BsArrowRight />}
+        x
+      />
     </LoginFormStyled>
   );
 }
 
 const LoginFormStyled = styled.div`
-  width: 90%;
   min-height: 430px;
+  width: 90%;
   box-shadow: 0 10px 15px 3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
   padding: 40px 50px 0px;
   border-radius: ${theme.borderRadius.extraRound};
-  
 
+  @media screen and (min-width: 1370px) {
+    width: 85%;
+  }
+  
   .login-modes {
     display: grid;
     grid-template-columns: 50% 50%;
     border-bottom: 1px solid ${theme.colors.greyLight};
-
     margin-bottom: 40px;
-  
+    
     span {
-        font-size: ${theme.fonts.size.XS};
-        color: ${theme.colors.greyDark};
-        margin: 5px 0 10px;
-        align-self: center;
-      }
+      font-size: ${theme.fonts.size.XS};
+      color: ${theme.colors.greyDark};
+      margin: 5px 0 10px;
+      align-self: center;
+    }
   }
 
   h2 {
@@ -88,6 +99,4 @@ const LoginFormStyled = styled.div`
   .icon {
     font-size: ${theme.fonts.size.P2};
   }
-
-
 `;
