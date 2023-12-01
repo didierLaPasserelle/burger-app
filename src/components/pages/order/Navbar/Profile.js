9ec8072e5a85542e3c0587427e39c319c6a912ce
Delaplacette <delaplacette_didier@yahoo.fr>
2023-12-01 16:01:17 +0100

    F17 : Order > Navbar > add style

--- src/components/pages/order/Navbar/Profile.js
+++ src/components/pages/order/Navbar/Profile.js
@@ -1,13 +1,12 @@
-import { BsPersonCircle } from "react-icons/bs";
-import { RiArrowDropDownLine } from "react-icons/ri";
 import styled from "styled-components";
 import { theme } from "../../../../theme";
 import OrderContext from "../../../../context/OrderContext";
 import { useContext, useState } from "react";
 import { dropDownFadeIn } from "../../../../theme/animations";
+import getBoxesConfig from "./navbarRightSideConfig";
 
-export default function Profile() {
-  const { username } = useContext(OrderContext);
+export default function Profile({ displayToastNotification }) {
+  const { username, toggleBasketVisibility } = useContext(OrderContext);
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -15,33 +14,113 @@ export default function Profile() {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const navbarBoxes = getBoxesConfig(
+    username,
+    dropDownMenu,
+    toggleBasketVisibility,
+    displayToastNotification
+  );
+
   return (
     <ProfileStyled>
-      <BsPersonCircle />
-      <p>{username}</p>
-      <button onClick={dropDownMenu}>
-        <RiArrowDropDownLine />
-      </button>
+      {navbarBoxes.map((box) => {
+        const boxStyle =
+          box.text === "Admin"
+            ? "box-style admin"
+            : box.text === "Panier"
+            ? "box-style panier"
+            : "box-style";
 
-      {isMenuOpen && (
-        <div className="dropdown-content">
-          <a href="#">Mes informations</a>
-          <a href="/">Déconnexion</a>
-        </div>
-      )}
+        return (
+          <div key={box.id}>
+
+            <div className={boxStyle} onClick={box.onClick}>
+              {box.icon}
+              <p>{box.text}</p>
+              <button onClick={box.onClick}>
+                {box.buttonIcon ? box.buttonIcon : ""}
+              </button>
+            </div>
+
+            {box.text === username && isMenuOpen && (
+              <div className="dropdown-content">
+                <a href="#">Mes informations</a>
+                <a href="/">Déconnexion</a>
+              </div>
+            )}
+            
+          </div>
+        );
+      })}
     </ProfileStyled>
   );
 }
 
 const ProfileStyled = styled.div`
-  border: 1px solid ${theme.colors.newDark};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 5px;
-  padding: 10px 10px;
-  border-radius: 10px;
   position: relative;
+  display: flex;
+  gap: 15px;
+
+  .gift-icon {
+    cursor: pointer;
+    position: absolute;
+    top: 35px;
+    right: 50px;
+    color: ${theme.colors.newDark};
+  }
+
+  .notification-badge {
+    position: absolute;
+    top: 28px;
+    right: 45px;
+    width: 12px;
+    height: 12px;
+    border-radius: 50%;
+    background-color: red;
+  }
+
+  .box-style {
+    border: 1px solid ${theme.colors.newDark};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+    padding: 10px 10px;
+    border-radius: 10px;
+    cursor: pointer;
+
+    &.admin {
+      background: ${theme.colors.newDark};
+      color: ${theme.colors.primary};
+
+      :hover {
+        color: ${theme.colors.white};
+        background-color: ${theme.colors.primary};
+        border: 1px solid ${theme.colors.white};
+        transition: 200ms ease-out;
+      }
+      :active {
+        color: ${theme.colors.primary};
+        background-color: ${theme.colors.white};
+      }
+    }
+    &.panier {
+      background: ${theme.colors.primary};
+      color: ${theme.colors.white};
+      border: ${theme.colors.primary};
+
+      :hover {
+        color: ${theme.colors.primary};
+        background-color: ${theme.colors.white};
+        border: 1px solid ${theme.colors.primary};
+        transition: 200ms ease-out;
+      }
+      :active {
+        color: ${theme.colors.white};
+        background-color: ${theme.colors.primary};
+      }
+    }
+  }
 
   p {
     font-weight: ${theme.fonts.weights.bold};
